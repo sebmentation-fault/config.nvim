b9928494c52bf1efaa853e6189e124ab5b90dee8
Sebastian Kjällgren <sebastiankjallgren@Sebastians-MacBook-Pro.local>
2024-06-11 10:35:12 +0100

    feat: add more lsps and fmts to config

--- init.lua
+++ init.lua
@@ -564,19 +564,15 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        -- See `:help lspconfig-all` for a list of all the pre-configured LSPs
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
-        -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
-        --
-        -- Some languages (like typescript) have entire language plugins that can be useful:
-        --    https://github.com/pmizio/typescript-tools.nvim
-        --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
-        -- tsserver = {},
-        --
-
+        rust_analyzer = {},
+        htmx = {},
+        tsserver = {},
+        cssls = {},
+        html = {},
         lua_ls = {
           -- cmd = {...},
           -- filetypes = { ...},
@@ -651,6 +647,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        go = { 'gofmt' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
@@ -848,6 +845,7 @@ require('lazy').setup({
         'rust',
         'python',
         'zig',
+        'css',
         'javascript',
         'typescript',
       },
